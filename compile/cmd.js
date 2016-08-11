aa380200003e569b409baca462310c7668bbe6ef
Martin Liberg <martin.liberg@izettle.com>
2016-08-11 16:44:34 +0200

    Compile takes an arg that is a path to a webpack config file

--- compile/cmd.js
+++ compile/cmd.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
+const path = require("path")
 const webpack = require("webpack")
 const baseConfig = require("./webpack.config")
-const inputConfig = require("../webpack")
+const inputConfig = require(path.join(process.cwd(), process.argv[2]));
 
 const config = Object.assign({}, baseConfig, inputConfig)
 
 webpack(config, (err, stats) => {
-  console.log('[webpack:build]', stats.toString({
+  console.log(stats.toString({
     chunks: false, // Makes the build much quieter
     colors: true
   }));
