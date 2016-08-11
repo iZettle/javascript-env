c14a65de93ec7d6debd6191aa2d50a2d890ae3bd
Martin Liberg <martin.liberg@izettle.com>
2016-08-11 14:23:20 +0200

    Update eslint config
    Using our modified airbnb config.

--- lint/index.js
+++ lint/index.js
@@ -4,7 +4,7 @@ const spawn = require('child_process').spawn
 const path = require('path')
 
 const command = path.join(process.cwd(), 'node_modules', '.bin', 'eslint')
-const baseArgs = ['--config', path.join(__dirname, '.eslintrc')]
+const baseArgs = ['--config', path.join(__dirname, '.eslintrc.json')]
 
 module.exports = function eslint (args) {
   return spawn(command, baseArgs.concat(args))
