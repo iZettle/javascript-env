75605a7a4eabba036efa9cac008533418fd12388
Martin Liberg <martin.liberg@izettle.com>
2016-08-11 11:14:27 +0200

    Refactor eslint command

--- eslint/index.js
+++ eslint/index.js
@@ -3,12 +3,10 @@
 const spawn = require('child_process').spawn
 const path = require('path')
 
-const ESLINT_CMD = path.join(process.cwd(), "node_modules", ".bin", "eslint");
+const command = path.join(process.cwd(), "node_modules", ".bin", "eslint")
+const args = ["--config", path.join(__dirname, ".eslintrc"), "."]
 
 module.exports = function eslint() {
-  return spawn(
-    ESLINT_CMD,
-    ["--config", path.join(__dirname, ".eslintrc"), "."]
-  );
+  return spawn(command, args);
 }
 
