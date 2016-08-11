497aaa68a04285f62c75c327ef589eaae247d4c7
Martin Liberg <martin.liberg@izettle.com>
2016-08-11 17:11:33 +0200

    Add loaders for json, svg, and scss files

--- compile/webpack.config.js
+++ compile/webpack.config.js
@@ -7,8 +7,21 @@ module.exports = {
       test: /\.js$/,
       loader: "babel",
       query: {
-        presets: ["es2015"]
+        presets: ["es2015", "react", "stage-1"]
       }
+    }, {
+      test: /\.json$/,
+      loader: "json"
+    }, {
+      test: /\.svg$/,
+      loader: "raw"
+    }, {
+      test: /\.scss$/,
+      loaders: [
+        "style",
+        "css?modules&localIdentName=[local]---[hash:base64:5]&sourceMap",
+        "sass?sourceMap"
+      ]
     }]
   },
   plugins: [
