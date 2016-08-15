6152da3381b66bc01e55dd32d09821a9adad846f
Martin Liberg <martin.liberg@izettle.com>
2016-08-15 11:25:54 +0200

    Let test program accept tests glob via javascript-env config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,14 +2,13 @@ const path = require("path")
 const jsEnvConfig = require("../config")
 const createWebpackConfig = require("../compile/webpack.config")
 
-const workingDir = path.join(process.cwd(), "!(node_modules)/**", "*test.js")
 const webpackConfig = createWebpackConfig(jsEnvConfig.compile)
 
 module.exports = function webpackTestConfig(config) {
   config.set({
     browsers: ["PhantomJS"],
     files: [
-      workingDir
+      jsEnvConfig.test.files
     ],
     frameworks: [
       "jasmine",
@@ -17,7 +16,7 @@ module.exports = function webpackTestConfig(config) {
       "es6-shim"
     ],
     preprocessors: {
-      [workingDir]: ["webpack", "sourcemap"]
+      [jsEnvConfig.test.files]: ["webpack", "sourcemap"]
     },
     reporters: [
       "jasmine-expect-jsx",
