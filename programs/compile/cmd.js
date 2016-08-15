f1fabae17098266596b60f3dffd18838347c3fed
Martin Liberg <martin.liberg@izettle.com>
2016-08-15 14:51:10 +0200

    Add production flag to compile program

--- programs/compile/cmd.js
+++ programs/compile/cmd.js
@@ -8,6 +8,25 @@ const webpackConfig = createWebpackConfig(jsEnvConfig.compile)
 
 if (process.argv.includes("--dev-server")) {
   devServer(webpackConfig)
+} else if (process.argv.includes("--production")) {
+  webpackConfig.devtool = "cheap-module-source-map"
+  webpackConfig.plugins.push(
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  )
+
+  webpack(webpackConfig, (err, stats) => {
+    console.log(stats.toString({
+      chunks: false, // Makes the build much quieter
+      colors: true
+    }))
+  })
 } else {
   webpack(webpackConfig, (err, stats) => {
     console.log(stats.toString({
