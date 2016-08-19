fd6af22d9e020efb3b76339cbd2aa5c93a3e29b8
Martin Liberg <martin.liberg@izettle.com>
2016-08-19 11:10:58 +0200

    Remove strange tests

--- programs/compile/compile-with-webpack.test.js
+++
@@ -1,8 +0,0 @@
-const compileWithWebpack = require("./compile-with-webpack")
-
-describe("compileWithWebpack()", () => {
-  it("should throw an error in no webpack config is passed", () => {
-    expect(compileWithWebpack).toThrow(new Error("No webpack config passed"))
-  })
-})
-
