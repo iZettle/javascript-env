190d292a66133980219c0b11f23b3c60b0493614
Joakim Jarsäter <joakim@jarsater.com>
2017-02-08 11:34:59 +0100

    add sasslint

--- javascript-env.js
+++ javascript-env.js
@@ -4,6 +4,9 @@ module.exports = {
   lint: {
     files: path.join(__dirname, "**", "*.js")
   },
+  sasslint: {
+    files: path.join(__dirname, "**", "*.scss")
+  },
   test: {
     files: path.join(process.cwd(), "!(node_modules)/**", "*test.js")
   }
