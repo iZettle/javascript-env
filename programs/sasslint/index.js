e91376dfaaa2805b4cc8b29b7a5e4eab3f68afc9
Joakim Jarsäter <joakim@jarsater.com>
2017-02-08 09:25:55 +0100

    pass file pattern to sasslint program

--- programs/sasslint/index.js
+++ programs/sasslint/index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 const path = require("path")
+const jsEnvConfig = require("../../config")
 
 const command = path.join(process.cwd(), "node_modules", ".bin", "sass-lint")
 const baseArgs = [
   "--config", path.join(__dirname, ".sass-lint.yml"),
-  "--verbose"
+  "--verbose",
+  "--no-exit",
+  jsEnvConfig.sasslint.files
 ]
 
 const sasslint = args => ({ command, args: baseArgs.concat(args) })
