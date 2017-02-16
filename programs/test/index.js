06eae4c2e0f0272fca0f2e13548b0c596f493feb
Janne Lönn <janne@izettle.com>
2017-02-16 12:32:07 +0100

    Minor improvements and updates
    This reverts commit 46c9dcde51483b8a172654d8491a42dd9a54ba05.

--- programs/test/index.js
+++ programs/test/index.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 const path = require("path")
+const jsEnvConfig = require("../../config")
 
 const command = path.join(process.cwd(), "node_modules", ".bin", "karma")
 const baseArgs = ["start", path.join(__dirname, "karma.conf.js")]
-const karma = args => ({ command, args: baseArgs.concat(args) })
+const karma = jsEnvConfig.test ? args => ({ command, args: baseArgs.concat(args) }) : null
 
 module.exports = karma
